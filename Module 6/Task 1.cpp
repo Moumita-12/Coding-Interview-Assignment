98d527cb6e8f79e8d9d3812c2ae9a495bdbde7d9
Moumita Shib <120558169+Moumita-12@users.noreply.github.com>
2024-01-06 22:27:03 +0600

    Create Task 1.cpp

---
+++ Module 6/Task 1.cpp
@@ -0,0 +1,33 @@
+  bool isValid(string s) {
+        stack <char> stk;
+        unordered_map<char, char>umap;
+        umap[')'] = '(';
+        umap['}'] = '{';
+        umap[']'] = '[';
+       
+    for(auto ch:s){
+        if( ch == '(' ||  ch == '{' || ch == '['){
+            stk.push(ch);
+        }
+
+        else {
+
+            if(stk.empty()){
+                return false;
+            }
+
+            char opening = stk.top();
+            stk.pop();
+
+            if(opening != umap[ch]){
+                    return false;
+            }
+        }
+    }
+
+        return stk.empty();
+        
+    }
+
+// Time complexiety = O(n)
+// space complexiety = O(n) , n = length of string
